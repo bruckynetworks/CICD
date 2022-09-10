4e8465982530ce4fa28c14bc5f6baac8642f8c1e
bruckynetworks <bruckynetworks@protonmail.com>
2022-09-10 12:48:55 -0700

    get_c change

--- get_config.py
+++ get_config.py
@@ -5,7 +5,7 @@ from nornir_utils.plugins.functions import print_result
 nr = InitNornir(config_file="inventory/config.yaml")
 
 def get_thing(task):
-    task.run(task=netconf_get_config, source="running", filter_type="xpath", path="/native/router")
+    task.run(task=netconf_get_config, source="running", filter_type="xpath", path="/native/interface")
 
 results = nr.run(task=get_thing)
 print_result(results)
