948dc0b60d7d7b567c52ed5ce7d774f8b353f8b6
bruckynetworks <bruckynetworks@protonmail.com>
2022-09-11 14:16:18 -0700

    creds mask

--- cnc_get_config_secreds.py
+++ cnc_get_config_secreds.py
@@ -2,6 +2,7 @@
 edit config, using just yaml files
 """
 
+import os
 from nornir import InitNornir
 from nornir_netconf.plugins.tasks import netconf_edit_config
 from nornir_utils.plugins.functions import print_result
@@ -23,9 +24,9 @@ def edit_nc_config_from_yaml(task):
 
 def main():
     nr = InitNornir(config_file="nc_inventory/nc_config.yaml")
-    results = nr.run(task=edit_nc_config_from_yaml)
     nr.inventory.defaults.username = os.getenv("USERNAME")
-    nr.inventory.defaults.password = os.getenv("PASSWORD")       
+    nr.inventory.defaults.password = os.getenv("PASSWORD")    
+    results = nr.run(task=edit_nc_config_from_yaml)
     print_result(results)
 
 
